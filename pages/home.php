e44c7ce69c28458846c30167f0f4f7c0e0ecff21
Abinntr <muhammadbintoro9@gmail.com>
2025-01-31 10:33:30 +0700

    kiww

---
+++ pages/home.php
@@ -0,0 +1,25 @@
+<div class="content-wrapper">
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Starter Page</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div >
+                        <div class="card-body">
+                        <h1>Selamat datang</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
